8eede58d16ba6c66a3e02a3312828985ec5db8d5
schivuk <hchivuku@andrew.cmu.edu>
2016-06-23 00:09:56 -0700

    Cleaned up code

--- data-collection/views.py
+++ data-collection/views.py
@@ -14,72 +14,43 @@ from base64 import b64decode
 
 @transaction.atomic
 def home(request):
-    context = {}
-    return render(request, 'data-collection/home.html', context)
+	'''Renders a static homepage for the server'''
+	context = {}
+	return render(request, 'data-collection/home.html', context)
 
 
 @transaction.atomic
 def storeGameLevelData(request):
+	'''Stores game data from the app for a single attempt at any level into
+	the centralized server database.
+
+	Currently, the following data is being sent from the app to be stored
+	(the name of the key in the POST request is listed in brackets): 
+	 - Username (userName)
+	 - Current Level (currentLevel)
+	 - Number of Crashes (crashCount)
+	 - Number of Steps (stepCount)
+	 - Time taken to complete level (timeElapsed)
+	 - Score in game level (score)
+	 - Coordinates and English representation of crash locations (crashLocations)
+	 - File names of the echoes played and number of times played (echoFileNames)
+	 '''
 	if request.method != 'POST':
 		raise Http404
 
-	#Get the data from the request
-	print 'The unique user identification is: ' + request.POST['userName']
-	print 'The Game Level is: ' + request.POST['currentLevel']
-	print 'The number of Steps taken is: ' + request.POST['stepCount']
-	print 'The number of Crashes: ' + request.POST['crashCount']
-	print 'The time elapsed: ' + request.POST['timeElapsed']
-	print 'The score obtained: ' + request.POST['score']
-	print 'The encrypted string: ' + request.POST['testEncrypt']
-	print 'The crash location data: ' + request.POST['crashLocations']
-
-	#Open the private key pem file and read key
-	fOpen = open('private.pem', 'r')
-	privateKey = fOpen.read()
-
-	#Decrypt the data
-	rsakey = RSA.importKey(privateKey)
-	
-	#Decode the encrypted string using base64 encoding
-	raw_cipher_data = b64decode(request.POST['testEncrypt'])
-
 	#Decrypt the encrypted string using private key and RSA
-	decrypted = rsakey.decrypt(raw_cipher_data)
-	
-	print 'The decrypted string: '
-	print decrypted
+	decrypted = decryptData(request.POST['testEncrypt'])	#For test purposes
 
 	#Parse the crash location information
-	echoLocsString = request.POST['crashLocations']
-	echoLocs = []
-	if echoLocsString != '':
-		echoLocsString = echoLocsString.split(';')
-		for echo in echoLocsString:
-			indices = echo.split(',')
+	echoLocs = parseCrashLocs(request.POST['crashLocations'])
 
-			newCrashLoc = CrashLocation(x=int(float(indices[0])),
-						  				y=int(float(indices[1])),
-						  				detail=indices[2])
-			newCrashLoc.save()
-			echoLocs.append(newCrashLoc)
+	#Get total echo count and print each echo details
+	echoCount = parseEchoFileNames(request.POST['echoFileNames'])
 
-	#Check if the user has played before, and has data saved previously
-	user = UserGamePerformance.objects.filter(name=request.POST['userName'])
-	if len(user) == 0:
-		user = UserGamePerformance(name=request.POST['userName'])
-		user.save()
-	else:
-		user = user[0]
+	#Get the data from the request and print (For debugging/testing). Comment if necessary.
+	printData(request, echoCount)
 
-	echoCount = 0
-	echoFileNames = request.POST['echoFileNames'].split(',')
-	for echoFile in echoFileNames:
-		echo = echoFile.split(':')
-		print 'Name of echo file played: ' + echo[0] + ', ' + 'Number of times played: ' + echo[1]
-		echoCount = echoCount + int(echo[1])
-
-	print 'Total number of echos played in this level: ' + str(echoCount)
-
+	#Save all data besides crash locations
 	newGameLevel = GameLevel(level = int(request.POST['currentLevel']),
 							 numSteps = int(request.POST['stepCount']),
 							 numCrashes = int(request.POST['crashCount']),
@@ -89,27 +60,72 @@ def storeGameLevelData(request):
 							 score = int(request.POST['score']))
 	newGameLevel.save()
 
-	print "saved the game level without crash locations"
-
+	#Save the crash locations
 	if echoLocs != []:
 		newGameLevel.crashLocs.add(*echoLocs)
 		newGameLevel.save()
 
-	print "saved the new game level"
+	#Check if the user has played before, and has data saved previously
+	user = UserGamePerformance.objects.filter(name=request.POST['userName'])
+	if len(user) == 0:
+		user = UserGamePerformance(name=request.POST['userName'])
+		user.save()
+	else:
+		user = user[0]
 
 	#Add the game to the user's games list
 	user.games.add(newGameLevel)
 
-	print "saved the game to the user's games"
-
-
-	#Should create a new user if does not exist, at the start of the game. Make a request to another method
-	#In this method, simply pull that user out, and add the new GameLevel object to his games attribute
-
-	#context = {'testData': 'testValue'}
-
-	#return JsonResponse({'testData': 'testValue'}, status=200)
 	return HttpResponse("Success", status=200)
+
+def decryptData(data):
+	'''Decrypts an encrypted input string sent from the app using the
+	RSA cryptosystem's corresponding private key'''
+	#Open the private key pem file and read key
+	fOpen = open('private.pem', 'r')
+	privateKey = fOpen.read()
+
+	#Import the private key
+	rsakey = RSA.importKey(privateKey)
+	
+	#Decode the encrypted string using base64 encoding
+	raw_cipher_data = b64decode(data)
+
+	#Decrypt the encrypted string using private key and RSA
+	decrypted = rsakey.decrypt(raw_cipher_data)
+
+	return decrypted
+
+def parseCrashLocs(data):
+	'''Parses the POST request input of the crash locations into a format
+	to be stored in the database models'''
+	echoLocs = []
+	if data != '':
+		echoLocsString = data.split(';')
+		for echo in echoLocsString:
+			indices = echo.split(',')
+
+			newCrashLoc = CrashLocation(x=int(float(indices[0])),
+						  				y=int(float(indices[1])),
+						  				detail=indices[2])
+			newCrashLoc.save()
+			echoLocs.append(newCrashLoc)
+
+	return echoLocs
+
+def parseEchoFileNames(data):
+	'''Parses and prints name of echoes played and number of times they were
+	played. Returns the total number of times all echoes were played.'''
+	echoCount = 0
+	echoFileNames = data.split(',')
+	for echoFile in echoFileNames:
+		echo = echoFile.split(':')
+		#Print details
+		print 'Name of echo file played: ' + echo[0] + ', ' + 'Number of times played: ' + echo[1]
+		echoCount = echoCount + int(echo[1])
+
+	return echoCount
+
 
 def generate_RSA(bits=2048):
     '''
@@ -135,11 +151,14 @@ def getPublicKey(request):
 
 	return JsonResponse(context, status=200)
 
-
-def getToken(request):
-
-	#TODO: Need to figure out how to get CSRF token and send back
-	#Just use JsonResponse on the context object to send back the JSON object with the token
-	context = {}
-
-	return HttpResponse("Success on the GET request", status=200)
+def printData(request, echoCount):
+	'''Prints the request and parsed data for debugging/testing'''
+	print 'The unique user identification is: ' + request.POST['userName']
+	print 'The Game Level is: ' + request.POST['currentLevel']
+	print 'The number of Steps taken is: ' + request.POST['stepCount']
+	print 'The number of Crashes: ' + request.POST['crashCount']
+	print 'The time elapsed: ' + request.POST['timeElapsed']
+	print 'The score obtained: ' + request.POST['score']
+	print 'The encrypted string: ' + request.POST['testEncrypt']
+	print 'The crash location data: ' + request.POST['crashLocations']
+	print 'Total number of echos played in this level: ' + str(echoCount)
